091e08a65fe3efcb4a13c420b59164e0d5e5171c
krustybr <carlos.poles@griffithuni.edu.au>
2017-10-27 22:51:40 +1000

    Initial Commit

---
+++ VowelMovement/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VowelMovement
+//
+//  Created by Carlos Poles on 27/10/17.
+//  Copyright © 2017 Carlos Poles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
